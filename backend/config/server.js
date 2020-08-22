3b998aacf40158175bef6bdd34e0924046a0d224
Andrey Kirichenko <andrey.kirichenko999@gmail.com>
2020-08-22 10:24:29 +0300

    added strapi headless CMS

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '892ac135a25cdbca9252523f1e212e25'),
+    },
+  },
+});
